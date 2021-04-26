227445168c99bbc17d34c5dab53ef4cce8ef2c25
Janderson Soares <jandinsm@gmail.com>
2021-04-26 16:24:09 -0300

    Adiciona algoritimo para tiar media.

--- introducao-a-javascript-e-logica-deprogramacao_1/bloco_4.2.js
+++ introducao-a-javascript-e-logica-deprogramacao_1/bloco_4.2.js
@@ -1,7 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-
-
+// Soma todos números da variavel "numbers".
 let somaNumbers = 0;
 
 for (indexSoma = 0; indexSoma < numbers.length; indexSoma ++){
@@ -9,3 +8,12 @@ for (indexSoma = 0; indexSoma < numbers.length; indexSoma ++){
 };
 
 console.log (somaNumbers);
+//.
+
+// Tira a média da variavel "numbers".
+
+let media = somaNumbers / (numbers.length);
+
+console.log (media);
+
+// .
