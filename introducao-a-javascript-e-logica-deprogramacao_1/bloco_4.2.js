8f9948ad6efc2c504e178964bb0372539363d445
Janderson Soares <jandinsm@gmail.com>
2021-04-26 15:52:31 -0300

    Soma de numeros

---
+++ introducao-a-javascript-e-logica-deprogramacao_1/bloco_4.2.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
+
+let somaNumbers = 0;
+
+for (indexSoma = 0; indexSoma < numbers.length; indexSoma ++){
+    somaNumbers = somaNumbers + numbers[indexSoma];
+};
+
+console.log (somaNumbers);
