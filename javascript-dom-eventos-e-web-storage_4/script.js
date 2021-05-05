6da691c7b2d720c7ede6af47eae9b16f7592cf0e
Janderson Soares <jandinsm@gmail.com>
2021-05-05 14:08:44 -0300

    Botao cor vermelha paragrafo

--- javascript-dom-eventos-e-web-storage_4/script.js
+++ javascript-dom-eventos-e-web-storage_4/script.js
@@ -1,16 +1,14 @@
-function colorPara(){
-    let list = document.querySelectorAll(".paragrafo");
-    let pr = document.querySelectorAll(".paragrafo");
-    if(pr.style.color == "red") {
-        for (let index = 0; index < list.length; index++) {
-            list[index].style.fontSize = "20px";
+// Seleciona os paragrafos
+let list = document.querySelectorAll(".paragrafo")
 
-        for (let index = 0; index < list.length; index++)
-            list[index].style.fontSize = "30px";       
-        }
-    }
-    for(let indexC = 0; indexC <pr.length; indexC++) {
-            pr[indexC].style.color = "red";
-    }
-    
+
+
+
+
+function colorPara(){
+
+        for (let index = 0; index < list.length; index++) {
+            list[index].style.color = "red";
+            }
 }
+
